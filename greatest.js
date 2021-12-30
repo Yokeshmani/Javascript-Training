ca2b2d0029526541984004dffbbfc165aa066e02
Yokeshmani <yokeshmani18@gmail.com>
2021-12-30 09:59:20 +0530

    Task Greatest

---
+++ greatest.js
@@ -0,0 +1,13 @@
+var a=10;
+var b=8;
+var c=15;
+
+if(a>b && a>c){
+console.log("a is greater than b and c");
+}
+else if(b>a && b>c){
+    console.log("b is greater than a and c");
+}
+else{
+    console.log("c is greater than a and b");
+}
